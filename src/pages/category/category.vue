06fc0b992a8559bd4a192046a11733f757bd0063
chenqiaoyun <cqy@zwcustom.com>
2022-07-08 09:36:50 +0800

    feat: 导入插件

--- src/pages/category/category.vue
+++ src/pages/category/category.vue
@@ -1,10 +1,11 @@
 <template>
 	<view>
-
+<uni-nav-bar title="导航栏组件" left-text="返回" right-text="设置" @clickLeft="back"></uni-nav-bar>
 	</view>
 </template>
 
 <script lang="ts">
+//使用插件---去官网下载插件，然后可以看对应的文档进行使用
 	export default {
 		data() {
 			return {
